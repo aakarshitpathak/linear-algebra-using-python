22d624cff749f7a3583b937ac2e08447cf3be239
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-25 00:17:38 +0530

    commit3

---
+++ vector/Row_vector.py
@@ -0,0 +1,7 @@
+# importing required library (numpy as np)
+import numpy as np
+# creating a row vector with 1 row and 3 columns using np.array() function
+row_vec = np.array([[4,-1,2]])
+print("A row vector\n",row_vec)
+# printing the shape of the vector
+print("Shape of vector=",row_vec.shape)
