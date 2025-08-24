75b285be87e10a1987539c5bcf8624b8a1fb4415
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-25 00:17:04 +0530

    commit2

---
+++ vector/column_vector.py
@@ -0,0 +1,7 @@
+# importing required library (numpy as np)
+import numpy as np
+# creating a column vector with 2 rows and 1 column using np.array() function
+col_vec = np.array([[4],[3]])
+print ("A column vector\n",col_vec)
+# printing the shape of the vector
+print("Shape of vector=",col_vec.shape)
