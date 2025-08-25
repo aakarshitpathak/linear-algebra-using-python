b70d9abf07dc32f950fe5bba44bc4a1c90682a82
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-26 00:04:39 +0530

    commit10

---
+++ matrix/identity_matrix.py
@@ -0,0 +1,8 @@
+# importing required libraries (numpy as np)
+import numpy as np
+#Creating an Identity matrix of size 2 using np.identity() function
+identity_matrix_1 = np.identity(2)
+print("Identity_matrix 1\n",identity_matrix_1)
+#Creating an Identity matrix of size 3 using np.eye() fucntion
+identity_matrix_2 = np.eye(3)
+print("\nIdentity_matrix 2\n",identity_matrix_2)
