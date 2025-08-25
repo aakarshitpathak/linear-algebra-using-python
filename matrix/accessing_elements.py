00194b8c52bb829478272d22918addf33918baad
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-26 00:03:38 +0530

    commit7

---
+++ matrix/accessing_elements.py
@@ -0,0 +1,8 @@
+# importing required libraries (numpy as np)
+import numpy as np
+# Creating a 3x2 Matrix "A"
+A = np.array([[2,-2],[3,1],[1,4]])
+
+# now accessing the element from above 2X2 matrix
+#selecting an element at row index 2 and column index 1
+print("Element of matrix A at index position [2,1] is", A[2,1])
