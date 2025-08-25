4488fa72db450cb3e52a5e9e4a901dd06f684bbf
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-26 00:03:06 +0530

    commit6

---
+++ vector/indexing.py
@@ -0,0 +1,13 @@
+# In python, the indexing starts from zero. 
+# You can select the second element of the vector 'v' as follows:
+'''v=[ 4 ]
+     [-1 ]
+     [ 2 ] i.e. v=-1'''
+
+# importing required libraries (numpy as np)
+import numpy as np
+vector=np.array([[4],[-1],[2]])
+print("The vector\n",vector)
+#Addressing a value of vector by index
+print("\nElement at position 1=",vector[1])
+
