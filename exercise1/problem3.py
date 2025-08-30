66e68a0d931c66ff11630613ccea638defa7d2a3
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-31 00:02:32 +0530

    commit14

---
+++ exercise1/problem3.py
@@ -0,0 +1,10 @@
+# Find the transpose of matrix C
+
+import numpy as np
+C = np.array([[2,0,0],
+              [0,5,0],
+              [0,0,9]])
+
+# finding the transpose of matrix C
+
+print("Transpose of matrix C is\n",np.transpose(C))
