789823c3050a1fa560fc5d2831403273c6d48a2e
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-02 00:05:06 +0530

    commit18

---
+++ exercise2/problem1.py
@@ -0,0 +1,9 @@
+# Determinant of A
+
+import numpy as np
+# creating matrix 
+matrix = np.array([[3,-1,2],
+                  [1,2,3],
+                  [4,1,1]])
+# finding determinant 
+print(np.linalg.det(matrix))
