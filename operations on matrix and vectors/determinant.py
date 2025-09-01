907fd2cbc1f00f92d1acb131c9278c4dd5d088e1
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-02 00:06:40 +0530

    commit23

---
+++ operations on matrix and vectors/determinant.py
@@ -0,0 +1,7 @@
+# importing required libraries (numpy as np)
+import numpy as np
+# Creating Matrix
+matrix = np.array([[10, 2], 
+                  [5, 2]])
+#Finding the determinant
+print(np.linalg.det(matrix))
