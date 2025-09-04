7e05664e825efc0fe2ecb8408015d6f360f22e0c
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-05 00:03:19 +0530

    commit33

---
+++ operations on matrix and vectors/vector_multiplication_using_scaler.py
@@ -0,0 +1,8 @@
+# importing required libraries (numpy as np)
+import numpy as np
+#Defining a Vector 'v' and scalar 's'
+v = np.array([[2],[-1],[3]])
+s = 2.5 
+#Scalar Vector Multiplication
+vector_mul = v*s 
+print("Vector: \n",v,"\nScalar:",s,"\nScalar Vector multiplication:\n",vector_mul)
